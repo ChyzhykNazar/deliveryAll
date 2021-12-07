715e1a6bb046f87281d01ee8689fb3e1e5b5f543
Nazar Chyzhyk <90036257+ChyzhykNazar@users.noreply.github.com>
2021-12-08 01:43:02 +0200

    addDB

---
+++ src/sample/Const.java
@@ -0,0 +1,31 @@
+package sample;
+
+public class Const {
+    public static final String ALL = "*";
+    public static final String CAR_TABLE = "car";
+    public static final String CAR_ID = "idcar";
+    public static final String CAR_TYPE = "type";
+    public static final String CAR_BRAND = "brand";
+    public static final String CAR_COLOR = "color";
+    public static final String CAR_NUMBER = "number";
+    public static final String DEPARTURE_TABLE = "departure";
+    public static final String CLIENT_ID = "idclient";
+    public static final String FORM_TABLE = "form";
+    public static final String FORM_ID_FORM = "idform";
+    public static final String FORM_ID_SENDER = "idsender";
+    public static final String FORM_ID_RECIPIENT = "idrecipient";
+    public static final String FORM_SIZE = "size";
+    public static final String FORM_WEIGHT = "weight";
+    public static final String FORM_CITY_OF_DEPARTURE = "cityofdeparture";
+    public static final String FORM_CITY_OF_RECEIPT = "cityofreceipt";
+    public static final String FORM_COST = "cost";
+    public static final String FORM_DATE_OF_DEPARTURE= "dateofdeparture";
+    public static final String PERSONAL_DATA_TABLE= "personaldata";
+    public static final String PERSONAL_DATA_ID_PERSONAL_DATA= "idpersonalData";
+    public static final String PERSONAL_DATA_LOGIN = "login";
+    public static final String PERSONAL_DATA_PASSWORD = "password";
+    public static final String PERSONAL_DATA_FIRST_NAME= "firstName";
+    public static final String PERSONAL_DATA_LAST_NAME= "lastName";
+    public static final String PERSONAL_DATA_PHONE= "phone";
+    public static final String PERSONAL_DATA_EMAIL= "email";
+}
